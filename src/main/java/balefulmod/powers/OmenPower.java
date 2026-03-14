54325b4cb639d04afd97b7144b74218011463f14
Joseph Rosso <jdr1652@rit.edu>
2026-03-13 21:27:54 -0400

    added AbruptPain card and helperfunctions for useing Omen

--- src/main/java/balefulmod/powers/OmenPower.java
+++ src/main/java/balefulmod/powers/OmenPower.java
@@ -1,8 +1,10 @@
 package balefulmod.powers;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.potions.EssenceOfDarkness;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static balefulmod.BalefulMod.makeID;
@@ -30,5 +32,65 @@ public class OmenPower extends BasePower{
     @Override
     public void updateDescription() {
         this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    private void preformAction(AbstractCreature t, AbstractCreature s, AbstractGameAction action, int amountToRemove) {
+        addToBot(action);
+        addToBot(new ReducePowerAction(t, s, POWER_ID, amountToRemove));
+    }
+
+    private void preformAction(AbstractCreature t, AbstractCreature s, AbstractGameAction action, boolean removeAll) {
+        addToBot(action);
+        if (removeAll) {
+            addToBot(new RemoveSpecificPowerAction(t, s, POWER_ID));
+        }
+    }
+
+    /**
+     * Checks if given AbstractCreature has Omen,
+     * Preforms action if ture.
+     * 1 Omen is removed from creature
+     *
+     * @param target Creature to check Omen of
+     * @param source Creature action is from
+     * @param action Action preformed if Creature has Omen
+     */
+    public static void omenAction(AbstractCreature target, AbstractCreature source, AbstractGameAction action) {
+        if (target.hasPower(POWER_ID)) {
+            OmenPower p = (OmenPower) target.getPower(POWER_ID);
+            p.preformAction(target,source,action,1);
+        }
+    }
+
+    /**
+     * Checks if given AbstractCreature has Omen, preforms action if ture.
+     * Declare a non-negative amount of omen to remove.
+     *
+     * @param target Creature to check Omen of
+     * @param source Creature action is from
+     * @param action Action preformed if Creature has Omen
+     * @param amountToRemove Amount of Omen removed from target, must be non-negative
+     */
+    public static void omenAction(AbstractCreature target, AbstractCreature source, AbstractGameAction action, int amountToRemove) {
+        if (target.hasPower(POWER_ID)) {
+            OmenPower p = (OmenPower) target.getPower(POWER_ID);
+            p.preformAction(target, source,action, amountToRemove);
+        }
+    }
+
+    /**
+     * Checks if given AbstractCreature has Omen, preforms action if ture.
+     * Can remove all Omen
+     *
+     * @param target Creature to check Omen of
+     * @param source Creature action is from
+     * @param action Action preformed if Creature has Omen
+     * @param removeAll If true, all Omen is removed from target, otherwise no Omen is removed
+     */
+    public static void omenAction(AbstractCreature target, AbstractCreature source, AbstractGameAction action, boolean removeAll) {
+        if (target.hasPower(POWER_ID)) {
+            OmenPower p = (OmenPower) target.getPower(POWER_ID);
+            p.preformAction(target, source,action, removeAll);
+        }
     }
 }
